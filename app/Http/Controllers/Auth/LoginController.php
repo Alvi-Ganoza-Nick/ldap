59900f0937d40e658392254c40dd5b2c4d68653d
AlviRichard <richardalvi@gmail.com>
2020-12-27 12:49:30 -0500

    email

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -42,7 +42,7 @@ class LoginController extends Controller
     protected function credentials(Request $request)
     {
         return [
-            'cn' => $request->get('username'),
+            'name' => $request->get('username'),
             'password' => $request->get('password'),
         ];
     }
